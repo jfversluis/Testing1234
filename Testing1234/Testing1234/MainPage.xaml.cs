157a0a0eb1fce34e6e7cd01e2bfdd46d05f18fd0
Gerald Versluis <gerald@verslu.is>
2022-01-25 19:59:23 +0100

    Initial commit

---
+++ Testing1234/Testing1234/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Testing1234
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            
+            camera.Shutter();
+        }
+    }
+}
+
